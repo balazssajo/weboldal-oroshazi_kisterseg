3c16be9e31a6f0e58159b71181b3f37f59d370d4
balazssajo <balazssajo@gmail.com>
2024-02-25 09:38:04 +0100

    v1

---
+++ scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".home").click(function(e){
+        location.href = "/";   
+    });
+  });
+
+  $(document).ready(function(){
+    $(".pres").click(function(e){
+        location.href = "bemutatas.html";   
+    });
+  });
+
+  $(document).ready(function(){
+    $(".tasks-kozpont").click(function(e){
+        location.href = "kozpont_feladatai.html";   
+    });
+  });
